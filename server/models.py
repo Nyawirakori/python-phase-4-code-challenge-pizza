bbd416f0a7123e466afbb8486c168b73d364acb0
Joan Nyawira Kori <korijoan171@gmail.com>
2025-07-03 06:06:53 +0300

    tests passing

--- server/models.py
+++ server/models.py
@@ -20,40 +20,12 @@ class Restaurant(db.Model, SerializerMixin):
     name = db.Column(db.String)
     address = db.Column(db.String)
 
-    # Getting all restaurants
-    def to_dict(self):
-        return {
-        'id': self.id,
-        'name': self.name,
-        'address': self.address,
-        }
-    
-    # Getting by id
-    def to_dict_with_pizzas(self):
-        return {
-        'id': self.id,
-        'name': self.name,
-        'address': self.address,
-        'restaurant_pizzas': [
-            {
-                'id': rp.id,
-                'price': rp.price,
-                'pizza_id': rp.pizza_id,
-                'restaurant_id': rp.restaurant_id,
-                'pizza': {
-                    'id': rp.pizza.id,
-                    'name': rp.pizza.name,
-                    'ingredients': rp.pizza.ingredients
-                }
-            } for rp in self.restaurant_pizzas
-        ]
-    }
-
     # add relationship
-    restaurant_pizzas = db.relationship("RestaurantPizza",back_populates="restaurant", cascade='all,  delete-orphan')
+    restaurant_pizzas = db.relationship("RestaurantPizza", back_populates="restaurant", cascade="all")
+    pizzas = association_proxy("restaurant_pizzas", "pizza")
 
     # add serialization rules
-    serialize_rules = ('-restaurant_pizzas.restaurant',)
+    serialize_rules = ("-restaurant_pizzas.restaurant",)
 
     def __repr__(self):
         return f"<Restaurant {self.name}>"
@@ -66,19 +38,12 @@ class Pizza(db.Model, SerializerMixin):
     name = db.Column(db.String)
     ingredients = db.Column(db.String)
 
-    #Getting all pizzas
-    def to_dict_all_pizzas(self):
-        return {
-        'id': self.id,
-        'name': self.name,
-        'ingredients': self.ingredients,
-        }
-
     # add relationship
-    restaurant_pizzas = db.relationship("RestaurantPizza", back_populates ="pizza", cascade='all,  delete-orphan')
+    restaurant_pizzas = db.relationship("RestaurantPizza", back_populates="pizza")
+    restaurants = association_proxy("restaurant_pizzas", "restaurant")
 
     # add serialization rules
-    serialize_rules = ('-restaurant_pizzas.pizza',)
+    serialize_rules =("-restaurant_pizzas.pizza",)
 
     def __repr__(self):
         return f"<Pizza {self.name}, {self.ingredients}>"
@@ -90,22 +55,25 @@ class RestaurantPizza(db.Model, SerializerMixin):
     id = db.Column(db.Integer, primary_key=True)
     price = db.Column(db.Integer, nullable=False)
 
-    restaurant_id = db.Column(db.Integer, db.ForeignKey('restaurants.id'))
-    pizza_id = db.Column(db.Integer, db.ForeignKey('pizzas.id'))
-
     # add relationships
-    restaurant = db.relationship("Restaurant", back_populates = "restaurant_pizzas")
-    pizza = db.relationship("Pizza", back_populates= "restaurant_pizzas")
+    restaurant_id = db.Column(db.Integer, db.ForeignKey("restaurants.id"))
+    pizza_id = db.Column(db.Integer, db.ForeignKey("pizzas.id"))
+    
+    restaurant = db.relationship("Restaurant", back_populates="restaurant_pizzas")
+    pizza = db.relationship("Pizza", back_populates="restaurant_pizzas")
+
+    
 
     # add serialization rules
-    serialize_rules = ('-restaurant.restaurant_pizzas', '-pizza.restaurant_pizzas',)
+    serialize_rules = ("-restaurant.restaurant_pizzas", "-pizza.restaurant_pizzas")
+    
 
     # add validation
     @validates("price")
-    def validate_prices(self, key, price):
-        if price is None or not (1 <= price <= 30):
-            raise ValueError("Price must be between 1 & 30")
-        return price
-    
+    def validate_price(self, key, value):
+        if not(1 <= value <= 30):
+            raise ValueError("Price must be between 1 and 30")
+        return value
+
     def __repr__(self):
         return f"<RestaurantPizza ${self.price}>"
